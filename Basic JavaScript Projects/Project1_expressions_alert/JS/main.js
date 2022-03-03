8585c26005f3bc01fe4e1a8cd57cdb17a1603f6a
Mia C <calibosomia@gmail.com>
2022-03-02 22:57:08 -0800

    Assignment#1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+var A="This is a string.<br><br>";
+var A= A.fontcolor("green"); //Assigned color to a variable
+var B="Welcome to my page!";
+var C="Good Attitude <br>", D=" Learn new things everytime.<br>";
+
+
+window.alert(B); //adding Variable A
+document.write(A); 
+
+document.write("Steve Jobs said, \"Everybody should learn to program a computer, because it teaches you how to think.\"");
+
+document.write("<br> <br>\"I think it's fair to say that personal computers have become the most empowering tool we\'ve ever created."
++"They\'re tools of communication, they\'re tools of creativity, and they can be shaped by their user.\""
++"- Bill Gates<br><br>"); //Concatenate a string
+
+document.write("Best Attitude as a Developer:"); document.write(D)
+
+document.write("Adding 5 and 5 is &nbsp; "); document.write(5+5)//Expression
+
