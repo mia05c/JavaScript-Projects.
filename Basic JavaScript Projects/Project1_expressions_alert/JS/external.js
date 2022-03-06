d3f6ccd886dd743d5f3677ec01c6b470107799e7
Mia C <calibosomia@gmail.com>
2022-03-06 00:14:37 -0800

    Project_2

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/external.js
@@ -0,0 +1,5 @@
+function My_First_Function () {
+    var str= "This text is Green!");
+    var result= str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML=result;
+}
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function First_Function () { // this function is for the button on click
+    var str = "Hello, World.";
+    var result=str.fontcolor ("blue");
+    document.getElementById("Blue_Text").innerHTML=result; 
+}
+
+function myFunction ()  { //this function is for Concatenate
+    var sentence = "I'm learning";
+    sentence += " a lot from this course";
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
